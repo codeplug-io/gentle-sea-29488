49f4a9a6959fed58bcbb3feff55c1a0898dd18df
AikGodwin <techplug.io@gmail.com>
2021-01-16 08:43:50 -0800

    removed the code res.send() to resolve routing during production

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send({ bye: 'buddy' })
-});
+// app.use( "/", () => {
+// 	res.send({ "bye": "world"});
+// });
 
+const path = require('path');
 if( process.env.NODE_ENV === 'production' ) {
 	//Express will serve up the index.html file
 	//like our main.js file, or main.css file!
@@ -12,14 +13,13 @@ if( process.env.NODE_ENV === 'production' ) {
 
 	//Express will serve up the index.html file
 	//if it doesnt recongnize the route
-	const path = require('path');
 	app.get( '*', (req, res) => {
-		     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html') );
+			 res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html') );
 			});
 	}else {
 		app.use( express.static('client/build') );
 
-		app.get( '/', (req, res) => {
+		app.get( '*', (req, res) => {
 			res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html') );
 		   });
 	}
